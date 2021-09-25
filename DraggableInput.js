85db0b2444543d10ac35561a8b79dd8c50ea1c57
Matt <kuposrock@gmail.com>
2021-09-25 13:59:14 -0700

    update

--- DraggableInput.js
+++ DraggableInput.js
@@ -2,52 +2,6 @@ import React, { Component } from 'react'
 
 export default class DraggableInput extends Component {
 
-<<<<<<< HEAD
-    constructor(props) {
-        super(props);
-        this.state = {x:0,y:0};
-      }
-
-    setHandleRef = ref => {
-        this.handleRef = ref;
-      }
-
-    passXY =() =>{
-
-    }
-      
-      initializeDrag = event => {
-        const {target, clientX, clientY} = event;
-        const { offsetTop, offsetLeft } = target;
-        const { left, top } = this.handleRef.getBoundingClientRect();
-        this.dragStartLeft = left - offsetLeft;
-        this.dragStartTop = top - offsetTop;
-        this.dragStartX = clientX;
-        this.dragStartY = clientY;
-        window.addEventListener('mousemove', this.startDragging, false);
-        window.addEventListener('mouseup', this.stopDragging, false);
-      }
-      
-      startDragging = ({ clientX, clientY }) => {    
-        this.handleRef.style.transform = `translate(${this.dragStartLeft + clientX - this.dragStartX}px, ${this.dragStartTop + clientY - this.dragStartY}px)`;
-        this.setState({x:this.dragStartLeft + clientX - this.dragStartX,y:this.dragStartTop + clientY - this.dragStartY})
-      }
-    
-      stopDragging = () => {
-        window.removeEventListener('mousemove', this.startDragging, false);
-        window.removeEventListener('mouseup', this.stopDragging, false);
-       
-        console.log(this.state);
-      }  
-
-    render() {
-        return (
-            <div className="memeText" onMouseDown={this.initializeDrag} ref={this.setHandleRef} contentEditable="false">
-                Edit and Drag Me
-            </div>
-        )
-    }
-=======
 
   constructor(props) {
     super(props);
@@ -84,7 +38,6 @@ export default class DraggableInput extends Component {
   stopDragging = () => {
     window.removeEventListener('mousemove', this.startDragging, false);
     window.removeEventListener('mouseup', this.stopDragging, false);
-
     console.log(this.state);
   }
 
@@ -96,5 +49,4 @@ export default class DraggableInput extends Component {
       </div>
     )
   }
->>>>>>> b95145ebdf48515b9b2a8e019c3c9b16c3ecdca8
 }
--- MemeChoices.js
+++ MemeChoices.js
@@ -12,16 +12,10 @@ class MemeChoices extends React.Component {
 
 
     render() {
-<<<<<<< HEAD
-        var dragComps=[];
-        for(var i=0; i<this.props.meme.box_count;i++){
-            dragComps.push( <DraggableInput key={i} />)
-=======
         var dragComps = [];
         //Adds text boxes to the meme.
         for (var i = 0; i < this.props.meme.box_count; i++) {
             dragComps.push(<DraggableInput key={i} />)
->>>>>>> b95145ebdf48515b9b2a8e019c3c9b16c3ecdca8
         }
 
         return (
