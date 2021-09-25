8571fe2d9e499c183d5213b9ad6d424cc3f0777b
Adrian Walker <awalker3365@gmail.com>
2021-09-25 14:28:25 -0400

    more comments

--- MemeGen.js
+++ MemeGen.js
@@ -13,7 +13,7 @@ class MemeGen extends React.Component {
         };
     }
 
-    //Picking up information from API. 
+    //Picking up information from API.
     componentDidMount() {
         this.setState({ loading: true })
         fetch("https://api.imgflip.com/get_memes")
@@ -37,28 +37,30 @@ class MemeGen extends React.Component {
     }
 
     changeListNum = (num) => {
-        if(this.state.listNumber > 0 || num > 0){
-        this.setState((prevState, props) => ({
-            listNumber: prevState.listNumber + num,
-        }));
+        if (this.state.listNumber > 0 || num > 0) {
+            this.setState((prevState, props) => ({
+                listNumber: prevState.listNumber + num,
+            }));
+        }
     }
-}
 
     render() {
         console.log(this.state.MasterMemeList[this.state.listNumber])
-        if (this.state.MasterMemeList.length >= 1){
-        var backgrounds = { backgroundImage: 'url(' + this.state.MasterMemeList[this.state.listNumber].url + ')' }
-        var title = this.state.MasterMemeList[this.state.listNumber].name;
-        meme = <MemeChoices background={backgrounds} listNumber={this.state.listNumber} memeTitle={title} meme={this.state.MasterMemeList[this.state.listNumber]} />
-    }
+        //If MasterMemeList comes back undefined do not run.
+        if (this.state.MasterMemeList.length >= 1) {
+            var backgrounds = { backgroundImage: 'url(' + this.state.MasterMemeList[this.state.listNumber].url + ')' }
+            var title = this.state.MasterMemeList[this.state.listNumber].name;
+            meme = <MemeChoices background={backgrounds} listNumber={this.state.listNumber} memeTitle={title} meme={this.state.MasterMemeList[this.state.listNumber]} />
+        }
         return (
             <div>
                 <div className="container">
                     <div className="title">Meme Generator</div>
-                {meme}
-                <button onClick={()=>{this.changeListNum(-1)}}>&#8592;</button>
-                {this.state.listNumber+1 + " / " + this.state.MasterMemeList.length}
-                <button onClick={()=>{this.changeListNum(1)}}>&#8594;</button>
+                    {meme}
+                    <button onClick={() => { this.changeListNum(-1) }}>&#8592;</button>
+                    {/*Adds 1 to the index */}
+                    {this.state.listNumber + 1 + " / " + this.state.MasterMemeList.length}
+                    <button onClick={() => { this.changeListNum(1) }}>&#8594;</button>
 
                     <form onSubmit={this.mySubmitHandler}>
 
